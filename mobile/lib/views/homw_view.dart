aa15833bf8ff9e8ea4cb47ee05bdd860053e1bee
Ahmed <an1977304@gmail.com>
2025-07-29 00:05:25 +0300

    create splash logo

---
+++ mobile/lib/views/homw_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomwView extends StatelessWidget {
+  const HomwView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
